fb5cfc6989447a961e7f0712b0d25db4d327710a
Boma G <51271912+BOMAURIEL@users.noreply.github.com>
2025-10-06 22:16:38 +0000

    fix for functions

--- src/app.js
+++ src/app.js
@@ -3,9 +3,29 @@ import "./style.css";
 
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const card = document.querySelector('.card');
+  const suit = generateRandomSuit();
+  card.classList.add(suit);
+  card.innerHTML = generateRandomNumber();
+  setTimeout(() => {
+    card.classList.remove(suit);
+    card.innerHTML = '';
+  }, 1000);
+};
+
+
+let generateRandomNumber = () => {
+  let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
